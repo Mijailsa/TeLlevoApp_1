b710c10236528d8ae0b171b52575dd22b6379030
Mijailsa <mija.salinas@duocuc.cl>
2022-09-02 00:22:49 -0400

    uwu

--- src/app/pages/recuperar/recuperar.page.ts
+++ src/app/pages/recuperar/recuperar.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-recuperar',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recuperar.page.scss'],
 })
 export class RecuperarPage implements OnInit {
+    rut: string;
+    correo: string;
 
-  constructor() { }
+  constructor(private usuarioService: UsuarioService, private router: Router, private toastController:ToastController) { }
 
   ngOnInit() {
   }
+
+ recuperar(){
+  var usuarioRecuperar = this.usuarioService.validarRecuperar(this.rut, this.correo)
+
+  if (usuarioRecuperar == true ) {
+    this.router.navigate(['/CambioContrasena'])
+    
+  }else{
+    this.toastError
+  }
+ }
+ async toastError() {
+  const toast = await this.toastController.create({
+    message: 'USUARIO O CORREO NO COINCIDEN',
+    duration: 3000
+  });
+  toast.present();
+}
 
 }
--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroPage implements OnInit {
 
-  constructor() { }
+  alumno = new FormGroup({
+    rut: new FormControl('', [Validators.required, Validators.pattern('[0-9]{1,2}.[0-9]{3}.[0-9]{3}-[0-9kK]{1}')]),
+    nombre: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    apellido: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    fecha_nac: new FormControl('', Validators.required),
+   correo: new FormControl('',Validators.email),
+    password: new FormControl('', [Validators.required, 
+                                   Validators.minLength(6),
+                                   Validators.maxLength(18)]),
+    tipo_usuario: new FormControl('')
+  });
+
+  verificar_password: string;
+  constructor(private usuarioService: UsuarioService, private router: Router) { }
 
   ngOnInit() {
   }
+  registrar(){
+    if (this.alumno.controls.password.value != this.verificar_password) {
+      alert('CONTRASEÑAS NO COINCIDEN!');
+      return;
+    }
+    this.usuarioService.agregarUsuario(this.alumno.value);
+    this.alumno.reset();
+    alert('USUARIO REGISTRADO!');
 
+    this.router.navigate(['/login']);
+  }
 }
